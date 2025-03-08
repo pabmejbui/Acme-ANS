0eab2d9d37ab3cd8f049f761e03c288c86804243
Fraperper5 <fraperper5@alum.us.es>
2025-03-08 14:31:30 +0100

    feat(student2): Task - 043 - Booking entity done!

---
+++ src/main/java/acme/entities/bookings/Booking.java
@@ -0,0 +1,77 @@
+
+package acme.entities.bookings;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.datatypes.Money;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidMoney;
+import acme.client.components.validation.ValidNumber;
+import acme.client.components.validation.ValidString;
+import acme.entities.customers.Customer;
+import acme.entities.passenger.Passenger;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Booking extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes----------------------------------------
+
+	@Mandatory
+	@ValidString(pattern = "^[A-Z0-9]{6,8}$")
+	@Automapped
+	private String				locatorCode;
+
+	@Mandatory
+	@ValidMoment(past = true)
+	@Temporal(TemporalType.TIMESTAMP)
+	@Automapped
+	private Date				purchaseMoment;
+
+	@Mandatory
+	@Enumerated(EnumType.STRING)
+	@Automapped
+	private TravelClass			travelClass;
+
+	@Mandatory
+	@ValidMoney
+	@Automapped
+	private Money				price;
+
+	@Optional
+	@ValidNumber(min = 4, max = 4)
+	@Automapped
+	private Integer				lastCardNibble;
+
+	// Derived attributes--------------------------------
+
+	// Relationships-------------------------------------
+
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	private Customer			customer;
+
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	private Passenger			passenger;
+
+}
