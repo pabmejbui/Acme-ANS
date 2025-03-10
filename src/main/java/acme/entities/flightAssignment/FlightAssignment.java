84c28b559d8169cee5801b544d2a0a7d2a3261e0
Pablo Mejias <pabmejbui@alum.us.es>
2025-03-10 08:10:26 +0100

    feat(student1): Task 022 - Flight entity #98

--- src/main/java/acme/entities/flightAssignment/FlightAssignment.java
+++ src/main/java/acme/entities/flightAssignment/FlightAssignment.java
@@ -16,6 +16,7 @@ import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidMoment;
 import acme.client.components.validation.ValidString;
+import acme.entities.flightCrewMembers.FlightCrewMember;
 import lombok.Getter;
 import lombok.Setter;
 
