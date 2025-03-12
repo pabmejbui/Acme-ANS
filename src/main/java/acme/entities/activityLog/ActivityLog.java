a672dedd2273887b2d4a6f926becdb935a5750a0
Jesus <Jesus@10.100.107.55>
2025-03-12 20:15:18 +0100

    fixed(Student3) Task008-R1- fixed activitylogEntity

--- src/main/java/acme/entities/activityLog/ActivityLog.java
+++ src/main/java/acme/entities/activityLog/ActivityLog.java
@@ -5,6 +5,7 @@ import java.util.Date;
 
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
+import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractEntity;
 import acme.client.components.mappings.Automapped;
@@ -48,6 +49,6 @@ public class ActivityLog extends AbstractEntity {
 
 	@Mandatory
 	@ManyToOne(optional = false)
-	@Automapped
+	@Valid
 	private FlightAssignment	flightAssignment;
 }
