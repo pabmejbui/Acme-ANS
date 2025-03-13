3202b34ed790cffcfa077826cb5c9191b4a45f94
Andrés García Rivero <andres7212@gmail.com>
2025-03-13 01:18:04 +0100

    feat(student5): add technician relationship and update draftMode type; include sample data for maintenance records

--- src/main/java/acme/entities/tasks/Task.java
+++ src/main/java/acme/entities/tasks/Task.java
@@ -13,6 +13,7 @@
 package acme.entities.tasks;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractEntity;
@@ -20,6 +21,7 @@ import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.ValidNumber;
 import acme.client.components.validation.ValidString;
+import acme.realms.Technician;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -53,8 +55,15 @@ public class Task extends AbstractEntity {
 	@Automapped
 	private Integer				estimatedDuration;
 
+	@Mandatory
 	@Automapped
+	private boolean				draftMode;
+
+	// Relationships  ---------------------------------------------------------
+
 	@Mandatory
 	@Valid
-	private Boolean				draftMode;
+	@ManyToOne
+	private Technician			technician;
+
 }
