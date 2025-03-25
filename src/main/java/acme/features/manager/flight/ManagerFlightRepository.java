2d1e2643b183d061e409c250eca1830927ec469f
Pablo Mejias <pabmejbui@alum.us.es>
2025-03-25 21:21:12 +0100

    fix(student1): fixing Task 082 - Follow Up. #536 - I have modified the authorise method on the show service to not allow front hacking as it has been suggested in the follow up.
    - As well, in the follow up, the professor recommended me to validate
    the supported currencys of the system. So it has been done in this

--- src/main/java/acme/features/manager/flight/ManagerFlightRepository.java
+++ src/main/java/acme/features/manager/flight/ManagerFlightRepository.java
@@ -2,11 +2,13 @@
 package acme.features.manager.flight;
 
 import java.util.Collection;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import acme.client.repositories.AbstractRepository;
+import acme.configuration.SystemConfiguration;
 import acme.entities.flights.Flight;
 import acme.entities.flights.Leg;
 
@@ -30,4 +32,7 @@ public interface ManagerFlightRepository extends AbstractRepository {
 
 	@Query("select l from Leg l where l.flight.id = :flightId")
 	Collection<Leg> findLegsByFlightId(int flightId);
+
+	@Query("SELECT sc FROM SystemConfiguration sc")
+	List<SystemConfiguration> findSystemConfiguration();
 }
