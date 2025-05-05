7fe8b9a72319c01942d0390f9b2f6cee5de65fe7
Fraperper5 <fraperper5@alum.us.es>
2025-05-05 19:22:04 +0100

    feat: Customer dashboard done!

--- src/main/java/acme/features/customer/customerDashboard/CustomerDashboardRepository.java
+++ src/main/java/acme/features/customer/customerDashboard/CustomerDashboardRepository.java
@@ -13,9 +13,9 @@ import acme.entities.bookings.BookingRecord;
 @Repository
 public interface CustomerDashboardRepository extends AbstractRepository {
 
-	@Query("SELECT b FROM Booking b WHERE b.customer.id =:customerId AND b.isPublished=true")
+	@Query("SELECT b FROM Booking b WHERE b.customer.id =:customerId AND b.draftMode=false")
 	Collection<Booking> findAllBookingsOf(int customerId);
 
-	@Query("SELECT bp FROM BookingRecord bp WHERE bp.booking.customer.id =:customerId AND bp.booking.dra=true")
+	@Query("SELECT bp FROM BookingRecord bp WHERE bp.booking.customer.id =:customerId")
 	Collection<BookingRecord> findAllBookingPassengerOf(int customerId);
 }
