c95d517b932d0c67c2963d3aaaa4430119df9755
Pablo Mejias <pabmejbui@alum.us.es>
2025-07-01 10:23:54 +0200

    fix(student1): Task 197 - Mistake in the flight entity #745

--- src/main/java/acme/constraints/flights/ValidTag.java
+++ src/main/java/acme/constraints/flights/ValidTag.java
@@ -1,19 +1,16 @@
 
 package acme.constraints.flights;
 
-import static java.lang.annotation.ElementType.FIELD;
-import static java.lang.annotation.RetentionPolicy.RUNTIME;
-
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 import javax.validation.Constraint;
 import javax.validation.Payload;
 
-@Target({
-	FIELD
-})
-@Retention(RUNTIME)
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = TagValidator.class)
 public @interface ValidTag {
 
