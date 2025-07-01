078a5945d7f3a9409b9de32ed6b59295e95fa7f6
Pablo Mejias <pabmejbui@alum.us.es>
2025-07-01 10:25:29 +0200

    fix(student1): Task 197 - Mistake in the flight entity #745 Third fix.

--- src/main/java/acme/constraints/flights/ValidTag.java
+++ src/main/java/acme/constraints/flights/ValidTag.java
@@ -9,7 +9,7 @@ import java.lang.annotation.Target;
 import javax.validation.Constraint;
 import javax.validation.Payload;
 
-@Target(ElementType.TYPE)
+@Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = TagValidator.class)
 public @interface ValidTag {
