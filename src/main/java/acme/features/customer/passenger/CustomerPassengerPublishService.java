0a1b4c7e54d783a36333c014f1a62d12f9b4dbba
Fraperper5 <fraperper5@alum.us.es>
2025-07-02 13:08:53 +0100

    fix: safe test suite passenger updated

--- src/main/java/acme/features/customer/passenger/CustomerPassengerPublishService.java
+++ src/main/java/acme/features/customer/passenger/CustomerPassengerPublishService.java
@@ -3,7 +3,6 @@ package acme.features.customer.passenger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
-import acme.client.components.models.Dataset;
 import acme.client.services.AbstractGuiService;
 import acme.client.services.GuiService;
 import acme.entities.passenger.Passenger;
@@ -60,9 +59,6 @@ public class CustomerPassengerPublishService extends AbstractGuiService<Customer
 
 	@Override
 	public void unbind(final Passenger passenger) {
-		Dataset dataset;
-
-		dataset = super.unbindObject(passenger, "fullName", "email", "passportNumber", "dateOfBirth", "specialNeeds", "draftMode");
-		super.getResponse().addData(dataset);
+		;
 	}
 }
