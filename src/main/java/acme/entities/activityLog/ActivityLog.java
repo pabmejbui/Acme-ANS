84fdc9be37409292a33c47197835966148d28995
Jesus <Jesus@10.100.107.55>
2025-07-03 22:20:12 +0200

    feat(Student3): Task-270-CreateIndices

--- src/main/java/acme/entities/activityLog/ActivityLog.java
+++ src/main/java/acme/entities/activityLog/ActivityLog.java
@@ -4,7 +4,9 @@ package acme.entities.activityLog;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.Index;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractEntity;
@@ -22,6 +24,9 @@ import lombok.Setter;
 @Getter
 @Setter
 @ValidActivityLog
+@Table(indexes = {
+	@Index(columnList = "flightAssignment_id"), @Index(columnList = "incidentType"), @Index(columnList = "draftMode"), @Index(columnList = "registrationMoment")
+})
 public class ActivityLog extends AbstractEntity {
 
 	private static final long	serialVersionUID	= 1L;
