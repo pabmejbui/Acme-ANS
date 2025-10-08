e6cc0f86413003d6fdebf2286c070971341764a0
Jesus <Jesus@10.100.206.94>
2025-10-08 12:34:29 +0200

    activitylog test

--- src/main/java/acme/features/flightCrewMember/activityLog/FlightCrewMemberActivityLogDeleteService.java
+++ src/main/java/acme/features/flightCrewMember/activityLog/FlightCrewMemberActivityLogDeleteService.java
@@ -71,7 +71,7 @@ public class FlightCrewMemberActivityLogDeleteService extends AbstractGuiService
 
 		member = (FlightCrewMember) super.getRequest().getPrincipal().getActiveRealm();
 		assignments = this.repository.findFlightAssignmentsByMemberIdOrPublished(member.getId());
-		selectedAssignments = SelectChoices.from(assignments, "flightLeg.flightNumberDigits", log.getFlightAssignment());
+		selectedAssignments = SelectChoices.from(assignments, "leg.flightNumber", log.getFlightAssignment());
 
 		dataset = super.unbindObject(log, "registrationMoment", "incidentType", "description", "severity", "draftMode");
 		dataset.put("assignments", selectedAssignments);
